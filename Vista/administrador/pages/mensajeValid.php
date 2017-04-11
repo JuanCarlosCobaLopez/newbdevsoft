a573d1ad48e1870940774cf6387afdb5694bb395
Juan Carlos Coba Lopez <juancarloscobalopez@gmail.com>
2017-04-10 22:29:15 -0400

    mensaje de validacion

---
+++ Vista/administrador/pages/mensajeValid.php
@@ -0,0 +1,18 @@
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Activación correcta</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    Ahora puedes acceder con tu cuenta.
+                        <p><a href="http://localhost/audisoft/Vista/visitante/index.html" class="btn btn-success">Continuar</a>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
