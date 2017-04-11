0815a4592b691836fc8a3b7a9c794010e46ef187
Juan Carlos Coba Lopez <juancarloscobalopez@gmail.com>
2017-04-10 22:16:53 -0400

    añadiendo validador de registro

--- Controlador/gestor registro usuario/validarDatosRegistro.php
+++ Controlador/gestor registro usuario/validarDatosRegistro.php
@@ -1,22 +1,62 @@
 <?php
 /*
-Este archivo verifica si el correo ya existe en la BD, si es así evuelve a la misma pagina sino comienza a actualizar la BD
+Este archivo verifica si el correo ya existe en la BD, si es así vuelve a la misma pagina sino comienza a actualizar la BD
 y envia a la pagina de usuario
 */
 session_start();
 include("../../Modelo/conectadorSQL.php");
-$correo=$_GET["correo"];
-$telf=$_GET["telf"];
-$password=$_GET["password"];
+require '../../librerias/PHPMailer/PHPMailerAutoload.php';
+$correo=$_POST["correo"];
+$nombre=$_POST["nombre"];
+$apellido=$_POST["apellido"];
+$telf=$_POST["telf"];
+$password=$nombre.rand(1999, 9999).$apellido;
+$descripcion=$_POST["descripcion"];
+$url=$_POST["url"];
+$fichero_subido = '/images/'.basename($_FILES['imagenNuevoEquipo']['name']);
+$ruta = '../../Vista/usuario/images/'.basename($_FILES['imagenNuevoEquipo']['name']);
+move_uploaded_file($_FILES['imagenNuevoEquipo']['tmp_name'], $ruta);
+$aleatorio=uniqid();
+
+//echo $password;
 //Se hace la consulta para saber si ese correo ya esta en la BD
+
 $queryCorreo = mysqli_query($conectador, "SELECT correo FROM usuario WHERE correo='".$correo."'");
+
 //Si no esta...
 if(mysqli_num_rows($queryCorreo)==0)
-	{
-		mysqli_query($conectador,"INSERT INTO `usuario` (`CORREO`, `TELEFONO`, `CONTRASENA`, `ESTADO`) VALUES ('".$correo."', '".$telf."', 
-			'".$password."', 'habilitado');");
-		//$_SESSION['pagina']='usuarios';
-		header("Location:../paginador/redireccionar.php?pag=usuarios");
+	{		
+	$mail = new PHPMailer();
+	$mail->isSMTP();
+	$mail->SMTPAuth = true;
+	$mail->SMTPSecure = 'tls';//Modificar
+	$mail->Host = 'smtp.gmail.com';//Modificar
+	$mail->Port = '587';//Modificar
+	$mail->Username = 'newbdev.soft@gmail.com'; //Modificar
+	$mail->Password = 'auditorium'; //Modificar
+	
+	$mail->setFrom('newbdev.soft@gmail.com', 'Confirmacion de la cuenta en el Sistema de Reservas UMSS');//Modificar
+	
+	$mail->addAddress($correo, 'Confirmacion de la cuenta en el Sistema de Reservas UMSS');//Modificar
+	
+	$mail->Subject = 'LE DAMOS LA BIENVENIDA AL SISTEMA DE RESERVAS';//Modificar
+	$mail->Body = 'Estos son tus datos de registro:<br>
+	<br>Usuario: '.$nombre.' '.$apellido.'
+	<br>Contraseña: '.$password.'
+	<br>Debes activar tu cuenta pulsando este enlace: http://localhost/audisoft/Controlador/activacion.php?id='.$aleatorio.'
+	<br>Gracias!
+	<br>'; //Modificar
+	$mail->IsHTML(true);
+	
+		if($mail->send()){
+
+mysqli_query($conectador,"INSERT INTO `usuario`(`CORREO`, `TELEFONO`, `CONTRASENA`, `estado`, `nombre`, `apellido`, `foto`, `descripcion`, `url`, `codigo`) 
+				VALUES ('".$correo."','".$telf."','".$password."','deshabilitado','".$nombre."','".$apellido."','".$fichero_subido."','".$descripcion."','".$url."','".$aleatorio."')");
+			    header("Location:../paginador/redireccionar.php?pag=mensaje envio codigo");
+			} else {
+			header("Location:../paginador/redireccionar.php?pag=mensaje error envio codigo");
+		}
+		
 	}
 //Si está...   
 else
@@ -24,4 +64,5 @@ else
 		//$_SESSION['pagina']='mensaje error registro usuario';
 		header("Location:../paginador/redireccionar.php?pag=mensaje error registro usuario");
 	}	
+
 ?>
