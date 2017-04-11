99fa2d2b1836f0850c11df83ffcd1ba42c57d3b1
Juan Carlos Coba Lopez <juancarloscobalopez@gmail.com>
2017-04-10 22:32:32 -0400

    archivo de mensaje simple

---
+++ Vista/usuario/pages/modificacionUsuarioValidada.php
@@ -0,0 +1,18 @@
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Mi Perfil</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    Modificación de tu perfil satisfactoriamente!
+                        <p><a href="../../../Controlador/paginador/redireccionarUsuario.php?pag=interfaz principal de usuario" class="btn btn-success">Continuar</a>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
