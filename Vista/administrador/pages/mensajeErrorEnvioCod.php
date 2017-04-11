60afdebbf8d7c1327ad5071e1884aa27340800a5
Juan Carlos Coba Lopez <juancarloscobalopez@gmail.com>
2017-04-10 22:28:05 -0400

    archivo simple de mensaje

---
+++ Vista/administrador/pages/mensajeErrorEnvioCod.php
@@ -0,0 +1,18 @@
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Error al enviar correo</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    Ha ocurrido un error y no se puede enviar el correo de validación.
+                        <p><a href="../../../Controlador/paginador/redireccionar.php?pag=regUsuario" class="btn btn-success">Continuar</a>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
