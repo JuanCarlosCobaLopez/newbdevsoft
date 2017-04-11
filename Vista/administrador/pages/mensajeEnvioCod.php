accc2e2fd23e5772c81fa087f5804229568edfa2
Juan Carlos Coba Lopez <juancarloscobalopez@gmail.com>
2017-04-10 22:27:37 -0400

    archivo de info simple

---
+++ Vista/administrador/pages/mensajeEnvioCod.php
@@ -0,0 +1,18 @@
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Validacion enviada</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    Se ha enviado un mensaje al correo electronico con el código de activación.
+                        <p><a href="../../../Controlador/paginador/redireccionar.php?pag=usuarios" class="btn btn-success">Continuar</a>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
