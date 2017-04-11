f162970058fbf052c9a072d790fb94363b9f3cfc
Juan Carlos Coba Lopez <juancarloscobalopez@gmail.com>
2017-04-10 22:28:44 -0400

    otro archivo de mensaje de error

---
+++ Vista/administrador/pages/mensajeErrorValid.php
@@ -0,0 +1,18 @@
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Error al validar la cuenta</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    Ocurró un error al validar la cuenta.
+                        <p><a href="http://localhost/audisoft/Vista/visitante/index.html" class="btn btn-success">Continuar</a>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
